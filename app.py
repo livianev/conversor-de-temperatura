08edab64c339fe8a7dad1f02ead69004f908ff87
Ana Lívia Nascimento | Brazil | Licks Attorneys <ana.nascimento@lickslegal.com>
2025-06-04 13:55:12 -0300

    primeira atualizacao

---
+++ app.py
@@ -0,0 +1,25 @@
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    fahrenheit = None
+    kelvin = None
+
+    if request.method == "POST":
+        try:
+            celsius = float(request.form["celsius"])
+            fahrenheit = (celsius * 9/5) + 32
+            kelvin = celsius + 273.15
+        except ValueError:
+            pass
+
+    return render_template("index.html", fahrenheit=fahrenheit, kelvin=kelvin)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
